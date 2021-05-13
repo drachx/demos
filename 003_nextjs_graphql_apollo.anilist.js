3e911d9a6aa7b706d1a73ffcaac116d57ffa1ec3
Drach X <46549822+drachx@users.noreply.github.com>
2021-05-13 15:45:52 +1200

    Create 003_nextjs_graphql_apollo.anilist.js

---
+++ 003_nextjs_graphql_apollo.anilist.js
@@ -0,0 +1,85 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+import { ApolloClient, InMemoryCache, useQuery, gql } from "@apollo/client";
+
+/*
+playground: https://www.graphqlbin.com/v2/new
+api: https://graphql-weather-api.herokuapp.com/
+others: https://apis.guru/graphql-apis/
+
+$ npx create-next-app weather
+$ cd weather
+$ npm i @apollo/client graphql
+$ npm run dev
+*/
+
+export async function getStaticProps() {
+    const client = new ApolloClient({
+        uri: "https://graphql.anilist.co",
+        cache: new InMemoryCache(),
+    });
+
+    const { data } = await client.query({
+        query: gql`
+        {
+            Page {
+                media {
+                    title {
+                        english
+                    }
+                    description
+                    coverImage {
+                        large
+                        medium
+                        color
+                    }
+                }
+            }
+        }
+        `,
+    });
+
+    return {
+        props: {
+            data: data.Page.media
+        },
+    };
+}
+
+/*
+title: data.title.english
+description: data.description
+image: data.coverImage.large
+color: data.coverImage.color
+*/
+
+export default function Home({ data }) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Anilist Graphql</title>
+                <meta name="description" content="Anilist Graphql" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>Anilist ({data.length})</h1>
+
+                <div className={styles.grid}>
+                    {data.map((data) => (
+                        <div className={styles.card}>
+                            <h2>{data.title.english}</h2>
+                            <img src={data.coverImage.large}></img>
+                            <p dangerouslySetInnerHTML={{ __html: data.description }}></p>
+                        </div>
+                    ))}
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                /;.
+            </footer>
+        </div>
+    );
+}
